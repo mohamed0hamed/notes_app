6873e2a497f3a5e6b900ed90d2d719d7c99095bf
Your Name <you@example.com>
2025-12-24 21:24:38 +0200

    create type adapter for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,18 @@
+
+
+import 'package:hive_flutter/hive_flutter.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
 class NoteModel
 {
+  @HiveField(0)
   final String title ;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String data;
+  @HiveField(3)
   final int color ;
 
   NoteModel({
