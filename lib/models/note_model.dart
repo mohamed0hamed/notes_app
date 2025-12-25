9b15310afcacd623ba1e950902d2c08c0d67e538
Your Name <you@example.com>
2025-12-25 02:07:40 +0200

    create add note method to add note cubit

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -4,21 +4,21 @@ import 'package:hive_flutter/hive_flutter.dart';
 part 'note_model.g.dart';
 
 @HiveType(typeId: 0)
-class NoteModel
+class NoteModel extends HiveObject
 {
   @HiveField(0)
   final String title ;
   @HiveField(1)
   final String subtitle;
   @HiveField(2)
-  final String data;
+  final String date;
   @HiveField(3)
   final int color ;
 
   NoteModel({
   required this.title,
   required this.subtitle,
-  required this.data,
+  required this.date,
   required this.color});
   
 }
